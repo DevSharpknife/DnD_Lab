66b83cdce05694f11f57771e3b0a7b528a03994a
svvishjs <andreww92@live.com>
2020-11-26 15:32:59 +0000

    added rooms and levels

---
+++ src/main/java/Levels/Rooms/Library.java
@@ -0,0 +1,11 @@
+package Levels.Rooms;
+
+import java.util.ArrayList;
+
+public class Library extends MasterRoom {
+
+    public Library(String name, ArrayList<Character> characters, ArrayList<Baddie> baddies, int treasure) {
+        super(name, characters, baddies, treasure);
+
+    }
+}
